b63f322259767a757246d71d383455cb462527fe
Navarre <jpnavarre@Navarres-iMac.local>
2024-07-09 18:52:33 -0400

    worked on updating lib folder of js files with code

--- Develop/lib/cli.js
+++ Develop/lib/cli.js
@@ -7,6 +7,8 @@ class CLI {
   run() {
 
     // TODO: Make an inquirer prompt to get text, textColor, shapeType, shapeColor data from user
+     // TODO: Create a shape object based on inquirer data
+    
     let inquirer = require('inquirer');
 
     inquirer.prompt([
@@ -21,41 +23,19 @@ class CLI {
         message: 'Enter text color:'
       },
       {
-        type: 'input',
+        type: 'list',
         name: 'sharpeType',
         message: 'Choice Shape:'
       },
       {
-        type: 'input',
         name: 'shapeColor',
-        message: 'Shape Color:'
+        message: 'Whats your shape color:'
       },
     ]).then(answers => {
       console.info(`Project name: ${answers.projectName}`);
     });
-    // TODO: Create a shape object based on inquirer data
-    let inquirer = require('inquirer');
 
-    inquirer.prompt([
-      {
-        type: 'input',
-        name: 'shapeName',
-        message: 'Enter the name of the shape:'
-      },
-      {
-        type: 'input',
-        name: 'shapeDescription',
-        message: 'Enter a description of the shape:'
-      }
-    ]).then(answers => {
-      const shape = {
-        name: answers.shapeName,
-        description: answers.shapeDescription,
-      };
-    
-      console.log(`Shape created: ${JSON.stringify(shape, null, 2)}`);
-    });
-    // TODO: Create a svg object and set text and textColor using user Data
+   
     const svg = document.createElementNS('/Challenges/D.OOP/Images/10-oop-homework-demo.png', 'svg');
     svg.setAttribute('width', '100');
     svg.setAttribute('height', '100');
@@ -68,9 +48,10 @@ class CLI {
     
     svg.appendChild(text);
     
-    document.body.appendChild(svg);
+    document.body.appendChild(svg); 
+    
+    // TODO: Create a svg object and set text and textColor using user Data
     // TODO: Set svg shape with shape object created above
-
     // TODO: Write your svg file
 
 module.exports = CLI;
